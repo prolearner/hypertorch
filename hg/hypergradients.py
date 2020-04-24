65bd7e02539d0f2eb4d1dd7fd97a4181b282482f
riccardo <riccardo.grazz@gmail.com>
2020-04-24 18:33:04 +0200

    major change to function names and parameters + added  preliminary jupyter notebook with a logistic regression example

--- hg/hypergradients.py
+++ hg/hypergradients.py
@@ -5,139 +5,139 @@ import traceback
 
 
 # noinspection PyUnusedLocal
-def reverse_unroll(ws, lmbd, outer_loss, set_grad=True):
-    o_loss = outer_loss(ws, lmbd)
-    grads = torch.autograd.grad(o_loss, lmbd, retain_graph=True)
+def reverse_unroll(params, hparams, outer_loss, set_grad=True):
+    o_loss = outer_loss(params, hparams)
+    grads = torch.autograd.grad(o_loss, hparams, retain_graph=True)
     if set_grad:
-        update_tensor_grads(lmbd, grads)
+        update_tensor_grads(hparams, grads)
     return grads
 
 
 # noinspection PyUnusedLocal
-def reverse(ws_t, lmbd, K, phi_t, outer_loss, tol=1e-10, set_grad=True):
-    ws_t = [[w.detach().requires_grad_(True) for w in ws] for ws in ws_t]
-    o_loss = outer_loss(ws_t[-1], lmbd)
-    grad_outer_w, grad_outer_lmbd = get_outer_gradients(o_loss, ws_t[-1], lmbd)
+def reverse(params_history, hparams, K, fp_map_history, outer_loss, tol=1e-10, set_grad=True):
+    params_history = [[w.detach().requires_grad_(True) for w in params] for params in params_history]
+    o_loss = outer_loss(params_history[-1], hparams)
+    grad_outer_w, grad_outer_hparams = get_outer_gradients(o_loss, params_history[-1], hparams)
 
     alphas = grad_outer_w
-    grads = [torch.zeros_like(w) for w in lmbd]
-    K = min(K, len(ws_t) - 1)
+    grads = [torch.zeros_like(w) for w in hparams]
+    K = min(K, len(params_history) - 1)
     for k in range(-2, -(K + 2), -1):
-        w_mapped = phi_t[k + 1](ws_t[k], lmbd)
-        bs = grad_unused_zero(w_mapped, lmbd, grad_outputs=alphas, retain_graph=True)
+        w_mapped = fp_map_history[k + 1](params_history[k], hparams)
+        bs = grad_unused_zero(w_mapped, hparams, grad_outputs=alphas, retain_graph=True)
         grads = [g + b for g, b in zip(grads, bs)]
-        alphas = torch_grad(w_mapped, ws_t[k], grad_outputs=alphas)
+        alphas = torch_grad(w_mapped, params_history[k], grad_outputs=alphas)
 
-    grads = [g + v for g, v in zip(grads, grad_outer_lmbd)]
+    grads = [g + v for g, v in zip(grads, grad_outer_hparams)]
     if set_grad:
-        update_tensor_grads(lmbd, grads)
+        update_tensor_grads(hparams, grads)
 
     return grads
 
 
-def fixed_point(ws, lmbd, K, phi, outer_loss, tol=1e-10, set_grad=True):
-    ws = [w.detach().requires_grad_(True) for w in ws]
-    o_loss = outer_loss(ws, lmbd)
-    grad_outer_w, grad_outer_lmbd = get_outer_gradients(o_loss, ws, lmbd)
+def fixed_point(params, hparams, K, fp_map, outer_loss, tol=1e-10, set_grad=True):
+    params = [w.detach().requires_grad_(True) for w in params]
+    o_loss = outer_loss(params, hparams)
+    grad_outer_w, grad_outer_hparams = get_outer_gradients(o_loss, params, hparams)
 
-    w_mapped = phi(ws, lmbd)
-    vs = [torch.zeros_like(w) for w in ws]
+    w_mapped = fp_map(params, hparams)
+    vs = [torch.zeros_like(w) for w in params]
     vs_vec = cat_list_to_tensor(vs)
     for k in range(K):
         vs_prev_vec = vs_vec
-        vs = torch_grad(w_mapped, ws, grad_outputs=vs, retain_graph=True)
+        vs = torch_grad(w_mapped, params, grad_outputs=vs, retain_graph=True)
         vs = [v + gow for v, gow in zip(vs, grad_outer_w)]
         vs_vec = cat_list_to_tensor(vs)
         if float(torch.norm(vs_vec - vs_prev_vec)) < tol:
             break
 
-    grads = torch_grad(w_mapped, lmbd, grad_outputs=vs, allow_unused=True)
-    grads = [g + v if g is not None else v for g, v in zip(grads, grad_outer_lmbd)]
+    grads = torch_grad(w_mapped, hparams, grad_outputs=vs, allow_unused=True)
+    grads = [g + v if g is not None else v for g, v in zip(grads, grad_outer_hparams)]
 
     if set_grad:
-        update_tensor_grads(lmbd, grads)
+        update_tensor_grads(hparams, grads)
 
     return grads
 
 
-def CG(ws, lmbd, K, phi, outer_loss, tol=1e-10, set_grad=True):
-    ws = [w.detach().requires_grad_(True) for w in ws]
-    o_loss = outer_loss(ws, lmbd)
-    grad_outer_w, grad_outer_lmbd = get_outer_gradients(o_loss, ws, lmbd)
+def CG(params, hparams, K, fp_map, outer_loss, tol=1e-10, set_grad=True):
+    params = [w.detach().requires_grad_(True) for w in params]
+    o_loss = outer_loss(params, hparams)
+    grad_outer_w, grad_outer_hparams = get_outer_gradients(o_loss, params, hparams)
 
-    w_mapped = phi(ws, lmbd)
+    w_mapped = fp_map(params, hparams)
 
-    def dphi_dw(xs):
-        JphiTv = torch_grad(w_mapped, ws, grad_outputs=xs, retain_graph=True)
-        return [v - j for v, j in zip(xs, JphiTv)]
+    def dfp_map_dw(xs):
+        Jfp_mapTv = torch_grad(w_mapped, params, grad_outputs=xs, retain_graph=True)
+        return [v - j for v, j in zip(xs, Jfp_mapTv)]
 
-    vs = CG_torch.cg(dphi_dw, grad_outer_w, max_iter=K, epsilon=tol)  # K steps of conjugate gradient
+    vs = CG_torch.cg(dfp_map_dw, grad_outer_w, max_iter=K, epsilon=tol)  # K steps of conjugate gradient
 
-    grads = torch_grad(w_mapped, lmbd, grad_outputs=vs)
-    grads = [g + v for g, v in zip(grads, grad_outer_lmbd)]
+    grads = torch_grad(w_mapped, hparams, grad_outputs=vs)
+    grads = [g + v for g, v in zip(grads, grad_outer_hparams)]
 
     if set_grad:
-        update_tensor_grads(lmbd, grads)
+        update_tensor_grads(hparams, grads)
 
     return grads
 
 
-def CG_normaleq(ws, lmbd, K, phi, outer_loss, tol=1e-10, set_grad=True):
-    ws = [w.detach().requires_grad_(True) for w in ws]
-    o_loss = outer_loss(ws, lmbd)
-    grad_outer_w, grad_outer_lmbd = get_outer_gradients(o_loss, ws, lmbd)
+def CG_normaleq(params, hparams, K, fp_map, outer_loss, tol=1e-10, set_grad=True):
+    params = [w.detach().requires_grad_(True) for w in params]
+    o_loss = outer_loss(params, hparams)
+    grad_outer_w, grad_outer_hparams = get_outer_gradients(o_loss, params, hparams)
 
-    w_mapped = phi(ws, lmbd)
+    w_mapped = fp_map(params, hparams)
 
-    def dphi_dw(xs):
-        JphiTv = torch_grad(w_mapped, ws, grad_outputs=xs, retain_graph=True)
-        v_minus_JphiTv = [v - j for v, j in zip(xs, JphiTv)]
+    def dfp_map_dw(xs):
+        Jfp_mapTv = torch_grad(w_mapped, params, grad_outputs=xs, retain_graph=True)
+        v_minus_Jfp_mapTv = [v - j for v, j in zip(xs, Jfp_mapTv)]
 
         # normal equation part
-        Jphiv_minus_JphiJphiTv = jvp(lambda _ws: phi(_ws, lmbd), ws, v_minus_JphiTv)
-        return [v - vv for v, vv in zip(v_minus_JphiTv, Jphiv_minus_JphiJphiTv)]
+        Jfp_mapv_minus_Jfp_mapJfp_mapTv = jvp(lambda _params: fp_map(_params, hparams), params, v_minus_Jfp_mapTv)
+        return [v - vv for v, vv in zip(v_minus_Jfp_mapTv, Jfp_mapv_minus_Jfp_mapJfp_mapTv)]
 
-    v_minus_Jphiv = [g - jphiv for g, jphiv in zip(grad_outer_w, jvp(
-        lambda _ws: phi(_ws, lmbd), ws, grad_outer_w))]
-    vs = CG_torch.cg(dphi_dw, v_minus_Jphiv, max_iter=K, epsilon=tol)  # K steps of conjugate gradient
+    v_minus_Jfp_mapv = [g - jfp_mapv for g, jfp_mapv in zip(grad_outer_w, jvp(
+        lambda _params: fp_map(_params, hparams), params, grad_outer_w))]
+    vs = CG_torch.cg(dfp_map_dw, v_minus_Jfp_mapv, max_iter=K, epsilon=tol)  # K steps of conjugate gradient
 
-    grads = torch_grad(w_mapped, lmbd, grad_outputs=vs, allow_unused=True)
-    grads = [g + v if g is not None else v for g, v in zip(grads, grad_outer_lmbd)]
+    grads = torch_grad(w_mapped, hparams, grad_outputs=vs, allow_unused=True)
+    grads = [g + v if g is not None else v for g, v in zip(grads, grad_outer_hparams)]
 
     if set_grad:
-        update_tensor_grads(lmbd, grads)
+        update_tensor_grads(hparams, grads)
 
     return grads
 
 
-def neumann(ws, lmbd, K, phi, outer_loss, tol=1e-10, set_grad=True):
+def neumann(params, hparams, K, fp_map, outer_loss, tol=1e-10, set_grad=True):
     # from https://arxiv.org/pdf/1803.06396.pdf,  should return the same gradient of fixed point K+1
-    ws = [w.detach().requires_grad_(True) for w in ws]
-    o_loss = outer_loss(ws, lmbd)
-    grad_outer_w, grad_outer_lmbd = get_outer_gradients(o_loss, ws, lmbd)
+    params = [w.detach().requires_grad_(True) for w in params]
+    o_loss = outer_loss(params, hparams)
+    grad_outer_w, grad_outer_hparams = get_outer_gradients(o_loss, params, hparams)
 
-    w_mapped = phi(ws, lmbd)
+    w_mapped = fp_map(params, hparams)
     vs, gs = grad_outer_w, grad_outer_w
     gs_vec = cat_list_to_tensor(gs)
     for k in range(K):
         gs_prev_vec = gs_vec
-        vs = torch_grad(w_mapped, ws, grad_outputs=vs, retain_graph=True)
+        vs = torch_grad(w_mapped, params, grad_outputs=vs, retain_graph=True)
         gs = [g + v for g, v in zip(gs, vs)]
         gs_vec = cat_list_to_tensor(gs)
         if float(torch.norm(gs_vec - gs_prev_vec)) < tol:
             break
 
-    grads = torch_grad(w_mapped, lmbd, grad_outputs=gs)
-    grads = [g + v for g, v in zip(grads, grad_outer_lmbd)]
+    grads = torch_grad(w_mapped, hparams, grad_outputs=gs)
+    grads = [g + v for g, v in zip(grads, grad_outer_hparams)]
     if set_grad:
-        update_tensor_grads(lmbd, grads)
+        update_tensor_grads(hparams, grads)
     return grads
 
 
-def exact(w_opt_f, lmbd, outer_loss, set_grad=True):
-    grads = torch_grad(outer_loss(w_opt_f(lmbd), lmbd), lmbd)
+def exact(w_opt_f, hparams, outer_loss, set_grad=True):
+    grads = torch_grad(outer_loss(w_opt_f(hparams), hparams), hparams)
     if set_grad:
-        update_tensor_grads(lmbd, grads)
+        update_tensor_grads(hparams, grads)
     return grads
 
 
@@ -151,25 +151,25 @@ def list_dot(l1, l2):  # extended dot product for lists
     return torch.stack([(a*b).sum() for a, b in zip(l1, l2)]).sum()
 
 
-def jvp(phi, ws, vs):
-    dummy = [torch.ones_like(phw).requires_grad_(True) for phw in phi(ws)]
-    g1 = grd(list_dot(phi(ws), dummy), ws)
+def jvp(fp_map, params, vs):
+    dummy = [torch.ones_like(phw).requires_grad_(True) for phw in fp_map(params)]
+    g1 = grd(list_dot(fp_map(params), dummy), params)
     return grd(list_dot(vs, g1), dummy)
 
 
-def get_outer_gradients(outer_loss, ws, lmbd, retain_graph=True):
-    grad_outer_w = grad_unused_zero(outer_loss, ws, retain_graph=retain_graph)
-    grad_outer_lmbd = grad_unused_zero(outer_loss, lmbd, retain_graph=retain_graph)
+def get_outer_gradients(outer_loss, params, hparams, retain_graph=True):
+    grad_outer_w = grad_unused_zero(outer_loss, params, retain_graph=retain_graph)
+    grad_outer_hparams = grad_unused_zero(outer_loss, hparams, retain_graph=retain_graph)
 
-    return grad_outer_w, grad_outer_lmbd
+    return grad_outer_w, grad_outer_hparams
 
 
-def get_grad_func_fixed_args(grad_f, fixed_k=None, fixed_phi=None):
+def get_grad_func_fixed_args(grad_f, fixed_k=None, fixed_fp_map=None):
     # noinspection PyUnusedLocal
-    def grad_func_fixed_args(w, lmbd, K, phi, outer_loss, set_grad=True):
+    def grad_func_fixed_args(w, hparams, K, fp_map, outer_loss, set_grad=True):
         K = K if fixed_k is None else fixed_k
-        phi = phi if fixed_phi is None else fixed_phi
-        return grad_f(w, lmbd, K, phi, outer_loss, set_grad=True)
+        fp_map = fp_map if fixed_fp_map is None else fixed_fp_map
+        return grad_f(w, hparams, K, fp_map, outer_loss, set_grad=True)
 
     return grad_func_fixed_args
 
@@ -178,9 +178,9 @@ def cat_list_to_tensor(list_tx):
     return torch.cat([xx.view([-1]) for xx in list_tx])
 
 
-def update_tensor_grads(lmbd, grads):
-    for l, g in zip(lmbd, grads):
-        l.grad += g
+def update_tensor_grads(hparams, grads):
+    for l, g in zip(hparams, grads):
+        l.grad = g
 
 
 def grad_unused_zero(output, inputs, grad_outputs=None, retain_graph=False, create_graph=False):
